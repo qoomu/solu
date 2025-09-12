aa044bb6054f0f085184e1e54668561bfd0c67ae
Farrell Rafi <farrell@odooabc.com>
2025-09-12 15:30:35 +0700

    Use global

--- database/main.mjs
+++ database/main.mjs
@@ -8,10 +8,10 @@ import nodeEndpoint from 'comlink/dist/esm/node-adapter.mjs';
 //const dirname = path.dirname(require.resolve('./worker.js'));
 const dirname = /*typeof __dirname === 'undefined' ? path.dirname(require.resolve('./worker.js')) :*/ path.join(process.env.SOLU_NEXTJS_ROOT || process.cwd(), './database');
 
-globals.soluPgWorkers = {};
+global.soluPgWorkers = {};
 
 export function createLocalWorker(table) {
-  if (table in globals.soluPgWorkers) return globals.soluPgWorkers[table];
+  if (table in global.soluPgWorkers) return global.soluPgWorkers[table];
   if (!(fs.existsSync(path.join(dirname, './data/')))) fs.mkdirSync(path.join(dirname, './data/'));
   const worker = Comlink.wrap(nodeEndpoint(new Worker(path.join(dirname, './worker.js'))))
   worker.init('pglite', path.join(dirname, './data/' + table), table);
